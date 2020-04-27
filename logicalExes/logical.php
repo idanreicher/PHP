b820485806a669c049c11cf84c314a8287a24ba0
unknown <idanreicher@gmail.com>
2020-04-28 01:56:04 +0300

    logicalphp

---
+++ logicalExes/logical.php
@@ -0,0 +1,76 @@
+<?php
+
+// Factorial of any number
+
+$num = 4;
+$fact = 1;
+for ($i = 1 ;$i <= $num; ++$i) {
+    $fact *= $i;
+}
+ //echo "factorial fo {$num} is {$fact}";
+
+// Fibonacci series
+function Fibonacci($number)
+{
+    if ($number == 0) {
+        return 0;
+    } elseif ($number == 1) {
+        return 1;
+    } else {
+        return (Fibonacci($number - 1)+Fibonacci($number - 2));
+    }
+}
+
+$number = 10;
+for ($counter = 0; $counter<$number;$counter++) {
+    // echo Fibonacci($counter),' ,';
+}
+
+//  Reverse of any number
+
+function numRverse($num)
+{
+    $rev=0;
+           
+    while ($num>=1) {
+        $re=$num%10;
+
+        $rev=$rev*10+$re;
+        
+        $num=$num/10;
+    }
+    echo $rev;
+}
+
+//numRverse(1325);
+
+// number is Prime or not.
+
+function isPrime($num)
+{
+    $check=0;
+    for ($i=2;$i<=($num/2);$i++) {
+        if ($num%$i==0) {
+            $check++;
+            if ($check==1) {
+                return $num . ' is not prime';
+            }
+        }
+    }
+    return $num . ' is prime';
+}
+
+//echo isPrime(7);
+// LEAP year or not
+
+function isLeapYear($year)
+{
+    if ($year%4==0) {
+        echo $year. ' is a leap year';
+    } else {
+        echo $year. ' is not a leap year';
+    }
+}
+
+
+isLeapYear(2008);
